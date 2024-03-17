a70c4c8214294913a5b5391b6633cd8d4eebde52
minji0626 <jo010163@naver.com>
2024-03-17 13:54:38 +0900

    .

--- oracle.sql
+++ oracle.sql
@@ -11,3 +11,19 @@ SELECT * FROM book;
 
 도서테이블에 있는 모든 출판사를 검색하세요.
 SELECT publisher FROM book;
+
+중복값을 제거하기
+SELECT DISTINCT(publisher) FROM book;
+
+가격이 20,000원 미만인 도서를 검색하시오.
+SELECT * FROM book WHERE price < 20000;
+
+가격이 10000원 이상 20000원 이하인 도서를 검색하세요.
+SELECT * FROM book WHERE price >= 10000 AND price <= 20000;
+SELECT * FROM book WHERE price BETWEEN 10000 AND 20000;
+
+출판사가 굿스포츠 혹은 대한미디어인 도서를 검색하시오.
+SELECT * FROM book WHERE publisher IN ( '굿스포츠' , '대한미디어');
+
+출판사가 굿스포츠 혹은 대한미디어가 아닌 도서를 검색하세요.
+SELECT * FROM book WHERE publisher NOT IN ('굿스포츠','대한미디어');
